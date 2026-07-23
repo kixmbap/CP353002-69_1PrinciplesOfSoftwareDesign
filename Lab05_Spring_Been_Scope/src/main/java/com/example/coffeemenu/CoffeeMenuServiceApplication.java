d79024484bf3b3237f32a3f0eecfab9e50f0a569
kixmbap <pwathkim@gmail.com>
2026-07-23 23:28:16 +0700

    Fix Lab05 from submodule to normal folder

---
+++ Lab05_Spring_Been_Scope/src/main/java/com/example/coffeemenu/CoffeeMenuServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.coffeemenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeMenuServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeMenuServiceApplication.class, args);
+    }
+}
